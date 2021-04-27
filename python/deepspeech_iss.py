2fd8615ae7ece544810729fe502b9476b7c76f52
Miguel Negrao <miguel.negrao@friendlyvirus.org>
2021-04-27 22:24:53 +0100

    update script

--- python/deepspeech_iss.py
+++ python/deepspeech_iss.py
@@ -7,6 +7,10 @@ import argparse
 import pathlib
 from pathlib import PurePath, Path
 
+def writeListToFile(stringList, path):
+    with open(path,'w') as f:
+            f.write("\n".join(stringList))
+
 parser = argparse.ArgumentParser(description='Transcribe audio files.')
 parser.add_argument('audio_files', metavar='PATH', nargs='+',
                     help='audio file')
@@ -14,10 +18,6 @@ parser.add_argument('--output_dir', '-d', required=True, help='output directory 
 parser.add_argument('--language', '-l',  required=True, help='either \'english\' or \'chinese\'')
 
 args = parser.parse_args()
-print(args.output_dir)
-print(args.language)
-print(str(args.audio_files))
-
 
 if not os.path.exists(args.output_dir): 
     os.mkdir(args.output_dir)
@@ -31,12 +31,17 @@ for (file, wav_file) in zip(args.audio_files, wav_files):
     subprocess.run(["ffmpeg", "-y", "-i", file, "-acodec", "pcm_s16le", "-ar", "16000",
                      "-ac", "1", "-vn", "-sn", wav_file] ,check=True)
 
+wavFileListPath = output_dir / "filelist.txt"
+writeListToFile(wav_files, wavFileListPath)
+
+print("Running inaSpeechSegmenter")
 #must change name on windows
 subprocess.run([str(script_directory / PurePath("ina_speech_segmenter/ina_speech_segmenter")), "-o", args.output_dir,
-                "-i"] + wav_files, check=True)
+                "-f", str(wavFileListPath)], check=True)
 
+print("Running deepspeech_csv")
 #must change name on windows
 model = str(script_directory.parent / "models" / PurePath(args.language) / PurePath("deepspeech.pbmm"))
 scorer = str(script_directory.parent / "models" / PurePath(args.language) / PurePath("deepspeech.scorer"))
 subprocess.run([str(script_directory / PurePath("deepspeech/deepspeech_csv")), "--model", model,
-                "--scorer", scorer, "--hide_segment_time"] + wav_files, check=True)
+                "--scorer", scorer, "--hide_segment_time", "--audio", str(wavFileListPath)], check=True)
